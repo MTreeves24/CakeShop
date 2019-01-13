7460e32585bd10ef77bd93d05ce6172ef45c5fca
Mike Treeves <mtreeves24@gmail.com>
2019-01-13 11:46:24 +0000

    general updates

--- cake.js
+++ cake.js
@@ -1,22 +1,11 @@
 // Inspiration: https://hummingbirdbakery.com/ 
 
-// const rightArrow = document.querySelector(".rightArrow")
-// const leftArrow = document.querySelector(".lefttArrow")
-// const img = document.querySelector(".img")
 
-// rightArrow.addEventListener("click", function(){
-//   img.classList(opacity: "0")
-// })
-
-
-// below is the plugin code
 const panels = document.querySelectorAll('.panel');
 function toggleOpen() {
-  console.log('Hello');
   this.classList.toggle('open');
 }
 function toggleActive(e) {
-  console.log(e.propertyName);
   if (e.propertyName.includes('flex')) {
     this.classList.toggle('open-active');
   }
